cb7afdb9371d080f3dab44131775663cac76a0a3
ladreravincejoseph <ladreravincejoseph@gmail.com>
2023-05-05 05:29:10 +0800

    Add media queries for additional responsiveness

--- app.js
+++ app.js
@@ -4,19 +4,8 @@ const worksButton = document.querySelector("#works-bottom");
 const contactButton = document.querySelector("#contact-bottom");
 
 menuButton.addEventListener("click", () => {
-  if (aboutButton.classList.contains("show")) {
-    aboutButton.classList.remove("show");
-  } else {
-    aboutButton.classList.add("show");
-  }
-  if (worksButton.classList.contains("show")) {
-    worksButton.classList.remove("show");
-  } else {
-    worksButton.classList.add("show");
-  }
-  if (contactButton.classList.contains("show")) {
-    contactButton.classList.remove("show");
-  } else {
-    contactButton.classList.add("show");
-  }
+  document.querySelector("#burger").classList.toggle("active");
+  aboutButton.classList.toggle("show");
+  worksButton.classList.toggle("show");
+  contactButton.classList.toggle("show");
 });
